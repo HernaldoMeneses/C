3996afbcdfc4409cce321733f445f46f2269929e
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-02-24 11:07:02 -0300

    Create 1-001.c

---
+++ 1-001.c
@@ -0,0 +1,52 @@
+/*
+Reserved Words in C
+*/
+
+//By Dennis Ritchie to Unix Version 5.0
+auto    
+double
+int
+struct
+break
+else
+long
+switch
+case
+register
+typedef
+char
+extern
+return
+union
+float
+short
+unsigned
+continue
+for
+default
+goto
+sizeof 
+do
+if
+static
+while
+
+//Upgrade By ANSI (American national Standards institute)
+enum
+const
+signed
+void
+volatile
+
+//Upgrade By Compilers 8088/8086
+asm
+_cs
+_ds
+_es
+_ss
+cdecl
+far
+huge
+interrupt
+near
+pascal
